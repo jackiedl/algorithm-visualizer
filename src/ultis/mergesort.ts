23bd1531f57dec7123f8f80cac0ddb95f04b3061
Jackie <luujackie98@gmail.com>
2023-10-02 21:32:00 -0700

    Update Merge Sort

--- src/ultis/mergesort.ts
+++ src/ultis/mergesort.ts
@@ -1,51 +1,37 @@
 export function getMergeAnimation(array:any){
   const animations:any = [];
-  let arr = mergeSort(array, animations);
+  console.log(array);
+  let arr = mergeSort([...array], animations);
+  console.log(arr);
   return {animation: animations, array: arr}
 }
-
-function mergeSort(arr:any, animation: any){
-  let sorted = Array.from(arr);
-  let n = sorted.length;
-  let buffer = new Array(n);
-
-  for (let size = 1; size < n; size *= 2){
-    for (let leftStart = 0; leftStart < n; leftStart += 2*size){
-      let left = leftStart;
-      let right = Math.min(left + size, n);
-      let leftLimit = right;
-      let rightLimit = Math.min(right + size, n);
-
-      merge(left, right, leftLimit, rightLimit, sorted, buffer, animation);
-    }
-    let temp = sorted;
-    sorted = buffer;
-    buffer = temp;
+function mergeSort(arr: any, animation:any):any{
+  if (arr.length <= 1){
+    return arr;
   }
-  return sorted;
+  const mid = Math.floor(arr.length / 2);
+  const left = arr.slice(0, mid);
+  const right = arr.slice(mid);
+  return merge(mergeSort(left, animation), mergeSort(right, animation), animation, arr);
 }
 
-function merge(left:number, right:number, leftLimit:number, rightLimit:number, sorted:any, buffer:any, animation: any) {
-  let i = left;
-  
-  //Compare the two sub arrays and merge them in the sorted order
-  while (left < leftLimit && right < rightLimit) {
-    if (sorted[left] <= sorted[right]) {
-      animation.push([i, left]);
-      buffer[i++] = sorted[left++];
+function merge(left:any, right:any, animation: any, arr: any) {
+  const result = [];
+  let count = 1;
+  console.log(count);
+  console.log("Left: " + left);
+  console.log("Right:" + right);
+  while (left.length && right.length) {
+    if (left[0] < right[0]) {
+      let l_shift = left.shift();
+      result.push(l_shift);
     } else {
-      animation.push([i, right]);
-      buffer[i++] = sorted[right++];
+      let r_shift = right.shift();
+      result.push(r_shift);
     }
+    console.log("Results:" + result);
+    count += 1;
   }
-
-  //If there are elements in the left sub arrray then add it to the result
-  while (left < leftLimit) {
-    buffer[i++] = sorted[left++];
-  }
-
-  //If there are elements in the right sub array then add it to the result
-  while (right < rightLimit) {
-    buffer[i++] = sorted[right++];
-  }
+   
+  return [...result, ...left, ...right];
 }
